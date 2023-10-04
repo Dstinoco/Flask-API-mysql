829f2c4e8d2e20c418893c87ca510258ab1634b6
Douglas Tinoco <douglas.bart@hotmail.com>
2023-10-03 21:53:20 -0300

    continuando CRUD

--- app.py
+++ app.py
@@ -6,10 +6,7 @@ import pandas as pd
 mydb = mysql.connector.connect(user='root', password='bart1234',
                               host='localhost',
                               database='db_carros')
-
 app = Flask(__name__)
-
-
 
 
 @app.route('/carros', methods=['GET'])
@@ -22,7 +19,6 @@ def consultar_carros():
     pandas_df = pandas_df.to_dict(orient='records')
     return jsonify(pandas_df[:5])
     
-
 @app.route("/carros/<int:id>", methods=['GET'])
 def obter_carros(id):
     my_cursor = mydb.cursor()
@@ -35,9 +31,7 @@ def obter_carros(id):
         if carro.get('id') == id:
             return jsonify(carro)
         
-
-
-@app.route("/vendas", methods=['POST'])
+@app.route("/carros", methods=['POST'])
 def incluir_carro():
     data = request.get_json()
     my_cursor = mydb.cursor()
@@ -45,10 +39,32 @@ def incluir_carro():
     values = (data['id'], data['marca'], data['modelo'], data['ano'] )
     my_cursor.execute(query, values)
     mydb.commit()
-    return jsonify({'message': 'Carro adicionado com sucerro'}), 201
+    return jsonify({'message': 'Carro adicionado com sucesso'}), 201
+
+@app.route("/carros/<int:id>", methods=['DELETE'])
+def excluir_carro(id):
+    my_cursor = mydb.cursor()
+    query = 'DELETE FROM carro WHERE id = %s'
+    my_cursor.execute(query, (id,))
+    mydb.commit()
+    return jsonify({'message': f'Carro de ID {id} removido com sucesso.'})
 
 
+@app.route('/carros/<int:id>', methods=['PUT'])
+def atualizar_carro(id):
+    data = request.json
+    marca = data.get('marca')
+    modelo = data.get('modelo')
+    ano = data.get('ano')
 
+    if modelo is None or marca is None or ano is None:
+        return jsonify({'message': 'Campos: marca, modelo e ano são Obrigatorios'})
+    my_cursor = mydb.cursor()
+    query = 'UPDATE carro SET modelo = %s, marca = %s, ano = %s WHERE id = %s'
+    values = (modelo, marca, ano, id)
+    my_cursor.execute(query, values)
+    mydb.commit()
+    return jsonify({'message': f'Carro com o ID {id} atualizado!'})
 
 
 app.run(debug=True)    
